19fefe226e4c06d1ae062e44ff0c0ed22f0741d2
Pavel Struhar <pstruhar@gmail.com>
2017-12-12 15:41:31 +0100

    Add log in app_init

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include "mgos.h"
+#include "esp_log.h"
 
 #include "btstack.h"
 
 enum mgos_app_init_result mgos_app_init(void) {
+  ESP_LOGI("App", "mgos_app_init");
   return MGOS_APP_INIT_SUCCESS;
 }
